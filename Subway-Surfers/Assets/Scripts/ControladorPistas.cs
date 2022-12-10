31f078c0027a5cd3aea02b3a683e5acfd3d83084
Aluno <adrielestoller@gmail.com>
2022-12-10 12:46:10 -0300

    arquivos Ayron ~ 10/12

---
+++ Subway-Surfers/Assets/Scripts/ControladorPistas.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControladorPistas : MonoBehaviour
+{
+    public List<GameObject> plataformas = new List<GameObject>();
+    public int offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (var i = 0; i < plataformas.Count; i++)
+        {
+          Instantiate(plataformas[1], new Vector3(0, 0, i * 86), transform.rotation);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
